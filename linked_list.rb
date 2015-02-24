f29ca4d199f82d93a5e0cbee62d595726b637d8f
Tyler Graham <Tyler@Tylers-MBP.Home>
2015-02-23 20:34:41 -0700

    completes iterative and recursive lists

--- linked_list.rb
+++ linked_list.rb
@@ -1,128 +1,123 @@
 class IterativeList
-  # A list using Iterative approaches
-  # uses looping structures like while, for
-  # key variables:
-  # list_head
-  # node_data
-  # next_node
 
-  attr_accessor :node, :list_head, :counter, :tail, :next_node
+  attr_accessor :node, :list_head, :counter, :tail, :next_node, :previous, :node_position
 
-  def initialize
-    #@list = []
-    @tail = tail
+   def initialize
+     @tail = tail
+     @previous = nil
+     @list_head = nil
+     @next_node = nil
+     @counter = 0
+     @node_position = 0
+   end
+
+   def pop
+     @tail = @tail.previous
+     @tail.next_node = nil
+   end
+
+   def position(index, node = @list_head)
+     return nil unless node
+     if index == 0
+       if node
+         node.data
+       else
+         nil
+       end
+     else
+       position(index - 1, node.next_node)
+     end
+   end
+
+   def add_node(node)
+     @counter += 1
+     if @list_head == nil
+       @node = Node.new(node)
+       @list_head = @node
+     else
+       @node.node_position += 1
+       while @node.next_node != nil
+         @node = @node.next_node
+       end
+       @tail = Node.new(node)
+       @tail.previous = @node
+       @node.next_node = @tail
+     end
+   end
+ end
+
+
+class Node
+
+  attr_accessor :data, :next_node, :previous, :node_position
+
+  def initialize(data)
+    @data = data
     @previous = nil
-    @list_head = nil
-    @next_node = nil
-    @counter = 0
+    @node_position = 0
   end
-
-  def pop
-    @tail = @tail.previous
-    @tail.next_node = nil
-  end
-
-  # def position(number)
-  #
-  # end
-  #
-  # @list_head.position = 0
-
-  def add_node(node)
-    @counter += 1
-    if @list_head == nil
-      @node = Node.new(node)
-      @list_head = @node
-      #@list << @list_head
-    else
-      while @node.next_node != nil
-        @node = @node.next_node
-        #@list << @node
-      end
-      @tail = Node.new(node)
-      @tail.previous = @node
-      @node.next_node = @tail
-      #@list << @node
-      #@counter += 1
-    end
-  end
-
-  # def count_nodes
-  #   if @list_head == nil
-  #     @counter = 0
-  #   else
-  #     while
-  #     @counter += 1
-  #   end
-  # end
-
-
-
-  # list_head = node_1
-  # node_2 = node_1.next_node
-
-# then "if something", list_head = node_2, etc.
-
-#Tasks:
-  # "append" elements to the end of the list
-  # Count the number of the elements in the list
-  # access the "tail" (last element) of the list
-  # "pop" an element from the end of the list
-  # access an element by numeric position (as with array)
-
-  # e.g. node_2 = node_1.next_node
-
-  # node_1 = Node.new(1)
-  # node_2 = Node.new(2)
-  # node_3 = Node.new(3)
-  # node_4 = Node.new(4)
-
-  # node_1.next = node_2
-  # node_2.next = node_3
-  # node_3.next = node_4
 
 end
 
 
 class RecursiveList
-  # A list using Recursive approaches
-  # requires the node to call on itself
-  # key variables:
-  # list_head
-  # node_data
-  # next_node
 
-  attr_reader :node_name
-  attr_accessor :list_head, :node_data, :next_node
+  attr_reader :head
+  attr_accessor :data, :link
 
   def initialize
-    #@node_name = node_name
-    #@data = data
-    #@node = node
-    @next_node = next_node
-    @list_head = list_head
+    @data = data
+    @link = link
   end
 
-end
-
-
-class Node
-
-  attr_reader :node_name
-  attr_accessor :next_node, :previous
-
-  def initialize(node)
-    @node_name = node
-    @next_node = nil
-    @previous = nil
+  def count(node = head)
+    if node.nil?
+      0
+    else
+      1 + count(node.next_node)
+    end
   end
 
-end
+  def push(data, node = head)
+    if node.nil?
+      @head = Node.new(data)
+    else
+      if node.next_node.nil?
+        node.next_node = Node.new(data)
+      else
+        push(data, node.next_node)
+      end
+    end
+  end
 
-  # A single node for either list type
-  # most methods will be in the other classes
-  # but some nodes will need to be manipulated here
-  # key variables?:
-  # list_head
-  # node_data
-  # next_node
+  def position(index, node = head)
+    return nil unless node
+    if index == 0
+      if node
+        node.data
+      else
+        nil
+      end
+    else
+      position(index - 1, node.next_node)
+    end
+  end
+
+  def pop(node = head)
+    if node.nil?
+      return nil
+    elsif node.next_node && node.next_node.next_node.nil?
+      node.next_node = nil
+    else
+      pop(node.next_node)
+    end
+  end
+
+  def find_tail(data, node = head)
+    if data == node.data && node.next_node == nil
+      true
+    else
+      false
+    end
+  end
+end
